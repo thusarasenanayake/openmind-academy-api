b89622d30db2e5c16c461dcf9744c31f2b39e230
Thusara Senanayake <thusarasenanayake.dev@gmail.com>
2021-10-05 18:51:19 +0530

    response messages modified

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -2,6 +2,26 @@ const database = require('../database/mongodb');
 const students = database.getCollection('openmind_academy', 'students');
 const Student = require('../models/Student');
 
+// ------ responses ------
+const setNotFound = {
+  status: 'fail',
+  data: [{ message: 'No documents found!' }],
+};
+
+const setSuccess = (result) => {
+  return {
+    status: 'success',
+    data: [result],
+  };
+};
+
+const setError = (err) => {
+  return {
+    status: 'error',
+    data: [err.message || err.name],
+  };
+};
+
 const student_index_all = async (req, res) => {
   const options = {
     sort: { ID: 1 },
@@ -11,23 +31,23 @@ const student_index_all = async (req, res) => {
   const cursor = await students.find({}, options);
 
   if ((await cursor.count()) === 0) {
-    res.status(404).json('No documents found!');
+    res.status(404).json(setNotFound);
     return;
   }
 
   const result = await cursor.toArray();
-  res.json(result);
+  res.json(setSuccess(result));
 };
 const student_index_one = async (req, res) => {
   const id = Number.parseInt(req.params.id);
 
   const result = await students.findOne({ ID: id }, { projection: { _id: 0 } });
   if (result === null) {
-    res.status(404).json('No documents matched the ID');
+    res.status(404).json(setNotFound);
     return;
   }
 
-  res.json(result);
+  res.json(setSuccess(result));
 };
 
 const student_delete = async (req, res) => {
@@ -35,19 +55,19 @@ const student_delete = async (req, res) => {
 
   const result = await students.deleteOne({ ID: id });
   if (result.deletedCount === 1) {
-    res.json('Successfully deleted one document.');
+    res.json(setSuccess('Successfully deleted one document.'));
     return;
   }
-  res.json('No documents matched the query. Deleted 0 documents.');
+  res.json(setNotFound);
 };
 
 const student_create = async (req, res) => {
   const student = new Student(req.body);
   try {
     await students.insertOne(student);
-    res.json('Successfully added one document.');
+    res.json(setSuccess('Successfully added one document.'));
   } catch (err) {
-    res.status(401).json(err);
+    res.status(401).json(setError(err));
   }
 };
 
@@ -61,12 +81,12 @@ const student_update = async (req, res) => {
       { $set: { ...student } }
     );
     if (result.matchedCount === 1) {
-      res.json('Successfully updated one document.');
+      res.json(setSuccess('Successfully updated one document.'));
       return;
     }
     res.json('No documents matched the query. Updated 0 documents.');
   } catch (err) {
-    res.status(401).json(err);
+    res.status(401).json(setError(err));
   }
 };
 
